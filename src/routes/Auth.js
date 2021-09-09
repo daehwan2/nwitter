871ebbe59cae9ae0eaac698826338ea5921eacf4
Daehwan2 <dhdh0482@naver.com>
2021-09-09 23:00:52 +0900

    #2AUTHENTICATION completed!

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,31 +1,69 @@
+import { authService } from 'fbase';
 import React, { useState } from 'react';
-
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, GithubAuthProvider,signInWithPopup } from 'firebase/auth';
 const Auth = () => {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const onChange = (event)=>{
-        const{
-            taget:{name,value},
-        }=event;
-        if(name==="email"){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error,setError] = useState("");
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+        if (name === "email") {
             setEmail(value);
-        }else if(name==="password"){
+        } else if (name === "password") {
             setPassword(value);
         }
     };
-    const onSubmit = (event) =>{
+    const onSubmit = async (event) => {
         event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                //create account
+                data = await createUserWithEmailAndPassword(authService, email, password)
+                    
+            } else {
+                // log in
+                data = await signInWithEmailAndPassword(authService, email, password)
+                    
+            }
+            console.log(data);
+        }catch(error){
+            setError(error.message);
+        }
+    }
+    const toggleAccount = ()=>{
+        return setNewAccount((prev)=>!prev);
+    }
+    const onSocialClick=async (event)=>{
+        const {
+            target:{name}
+        }=event;
+        let provider;
+
+        if(name === "google"){
+            provider = new GoogleAuthProvider();
+        }else if(name ==="github"){
+            provider = new GithubAuthProvider();
+        }
+        const data = await signInWithPopup(authService,provider);
+        console.log(data);
     }
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
-                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value="Log In" />
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+                <input type="submit" value={newAccount ? "Create Acount" : "Log In"} />
             </form>
+            {error}
+            <span onClick={toggleAccount}>{newAccount ? "Log In" : "Create Account"}</span>
+
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
             </div>
         </div>
     );
