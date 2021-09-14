095db6860b3db9e00d9e0d93dde04d3b69a70444
Daehwan2 <dhdh0482@naver.com>
2021-09-14 23:00:26 +0900

    #5 EDIT PROFILE

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,50 @@
-import { authService } from 'fbase';
-import React from 'react';
+import { authService, dbService } from 'fbase';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import {doc, collection, addDoc,getDocs, query, where, orderBy} from 'firebase/firestore';
+import Nweet from 'components/Nweet';
+import { updateProfile } from 'firebase/auth';
 
-export default () => {
+export default ({userObj,refreshUser}) => {
+    const [nweets,setNweets]=useState([]);
+    const [newDisplayName, setNewDisplayName]=useState(userObj.displayName);
     const history = useHistory();
     const onLogOutClick = ()=>{
         authService.signOut();
         history.push('/');
     }
-    return (<>
-        <button onClick={onLogOutClick}>Log Out</button></>
+    const getMyNweets = async()=>{
+        const q = query(
+            collection(dbService,"nweets"),
+            where("creatorId","==",userObj.uid),
+            orderBy("createdAt","desc")
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc)=>console.log(doc.id,"=>",doc.data()));
+    };
+    useEffect(()=>{
+        getMyNweets();
+    },[]);
+    const onSubmit = async (event)=>{
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName){
+            await updateProfile(authService.currentUser,{
+                displayName:newDisplayName                
+            });
+            refreshUser();
+        }
+    }
+    const onChange = (event)=>{
+        const {target:{value}}=event;
+        setNewDisplayName(value);
+    }
+    return (
+        <>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Display Name" onChange={onChange} value={newDisplayName}/>
+            <input type="submit" value="Update Profile"/>
+        </form>
+        <button onClick={onLogOutClick}>Log Out</button>
+        </>
     );
 }
